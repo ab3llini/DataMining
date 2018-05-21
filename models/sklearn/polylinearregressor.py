9c47d146214a71bcef4b5ee9c0c76263042c56ef
ab3llini <albertomario.bellini@mail.polimi.it>
2018-05-21 14:59:15 +0200

    Added predicted customers for jan-fed 2018 with poly regressor deg 2, R2 = 0.875

--- models/sklearn/polylinearregressor.py
+++ models/sklearn/polylinearregressor.py
@@ -2,6 +2,7 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn import linear_model
 import models.sklearn.setbuilder as sb
 import models.sklearn.evaluator as eval
+import pandas as pd
 
 # LINEAR REGRESSOR, DEG = 2
 # TRAINING SET = mean_var_pre_imputed.csv
@@ -16,7 +17,7 @@ poly_degree = 2
 
 # Performs simple linear regression
 print("Linear regression started, polynomial degree = %s" % poly_degree)
-
+x = PolynomialFeatures()
 poly = PolynomialFeatures(degree=poly_degree)
 xtr_ = poly.fit_transform(data.xtr)
 xts_ = poly.fit_transform(data.xts)
@@ -24,5 +25,10 @@ xts_ = poly.fit_transform(data.xts)
 clf = linear_model.LinearRegression()
 clf.fit(xtr_, data.ytr)
 ypred = clf.predict(xts_)
+
+
+# frame = pd.DataFrame(ypred)
+
+# frame.to_csv('predicted_customers_jan_feb_2018.csv')
 
 print('R2 = %s' % eval.evaluate(data.yts, ypred))
