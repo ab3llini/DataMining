d6e6bd2d2560ce822263988d80ed74caf0488211
ab3llini <albertomario.bellini@mail.polimi.it>
2018-05-25 15:20:35 +0200

    Moved setbuilder in dataset

--- models/sklearn/decisiontreeregressor.py
+++ models/sklearn/decisiontreeregressor.py
@@ -2,18 +2,16 @@ from sklearn import tree
 import dataset.setbuilder as sb
 import models.sklearn.evaluator as eval
 
+print("Plain Decision regression tree without bagging")
 
 # Build training & test sets
 data = sb.SetBuilder(target='NumberOfSales', autoexlude=True).build()
 
 # Performs simple linear regression
-print("Decision regression tree")
 
 dtree = tree.DecisionTreeRegressor()
 dtree.fit(data.xtr, data.ytr)
 ypred = dtree.predict(data.xts)
-
-# ds.save_dataset(pd.DataFrame(ypred), 'customer_pred_jan_feb_LR_DEG1.csv')
 
 print('R2 = %s' % eval.evaluate(data.yts, ypred))
 
