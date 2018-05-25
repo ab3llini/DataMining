9bb84c9f38bbd90040f74f6ca3c7d6e883bf5dca
emilioC33 <emilio.capo@mail.polimi.it>
2018-05-25 12:18:01 +0200

    I like graphs, so here's more

--- dataexploration/graphs/graphs.py
+++ dataexploration/graphs/graphs.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from dataset.utility import get_frame_in_range
 from dataset.dataset import to_numpy
+from dataset.dataset import values_of
 
 def scatterplot(df, x=None, y=None, colour=None, regression=False):
     if x is None or y is None:
@@ -35,23 +36,8 @@ def boxplot(df, x, y, jitter=False):
     sb.swarmplot(x, y, data=df, color=".25")
     plt.show()
 
-def monthlysalesplot(df, bm, by, em, ey):
-    month_x = []
-    sales_y = []
-    while bm != em or by != ey:
-        dfframe = get_frame_in_range(df, bm, by, bm, by)
-        month_x.append(str(bm) + "-" + str(by))
-        sales_y.append(dfframe["NumberOfSales"].sum())
-        bm += 1
-        if bm == 13:
-            bm = 1
-            by += 1
 
-    sb.barplot(x=month_x, y=sales_y, hue_order=None).set_title("Monthly Number of Sales (All shops)")
-    plt.show()
-
-
-def monthlycustomersplot(df, bm, by, em, ey):
+def monthlyplot(df, bm, by, em, ey, target="NumberOfSales"):
     if em == 12:
         em = 1
         ey += 1
@@ -63,13 +49,13 @@ def monthlycustomersplot(df, bm, by, em, ey):
     while bm != em or by != ey:
         dfframe = get_frame_in_range(df, bm, by, bm, by)
         month_x.append(str(bm) + "-" + str(by))
-        sales_y.append(dfframe["NumberOfCustomers"].sum())
+        sales_y.append(dfframe[target].sum())
         bm += 1
         if bm == 13:
             bm = 1
             by += 1
 
-    sb.barplot(x=month_x, y=sales_y, hue_order=None).set_title("Monthly Number of Customers (All shops)")
+    sb.barplot(x=month_x, y=sales_y, hue_order=None).set_title("Monthly " + target +" (All shops)")
     plt.show()
 
 
@@ -80,6 +66,7 @@ def opendaybeforegeneralplot(df, storeID):
     dftoplot = pd.DataFrame(np.array(IsOpenList).reshape(523021, 1), columns=["OpenDayBefore"])
     dftoplot = df.assign(OpenDayBefore=dftoplot)
     dftoplotpershop = dftoplot[dftoplot["StoreID"] == storeID]
+    dftoplotpershop = dftoplot[dftoplot["IsOpen"] == 1]
     sb.boxplot(x="OpenDayBefore", y="NumberOfSales", data=dftoplotpershop).set_title("Sales / Shop Availability")
     plt.show()
 
@@ -94,9 +81,54 @@ def opendaybeforeonweekplot(df, storeID):
     dftoplot = pd.DataFrame(np.array(IsOpenList).reshape(448375, 1), columns=["OpenDayBefore"])
     dftoplot = df.assign(OpenDayBefore=dftoplot)
     dftoplotpershop = dftoplot[dftoplot["StoreID"] == storeID]
+    dftoplotpershop = dftoplot[dftoplot["IsOpen"] == 1]
     sb.boxplot(x="OpenDayBefore", y="NumberOfSales", data=dftoplotpershop).set_title("Sales / Shop Availability (-Sun)")
     plt.show()
 
+
+# If storeID = 0, ignore shop selection.
+def competitorplot(df, target="NumberOfSales"):
+    title = target + " / Nearest Competitor"
+    sb.barplot(x="NearestCompetitor", y=target, data=df).set_title(title)
+    plt.show()
+
+
+def frequencypershop(df, storeID, target="NumberOfSales", daily=False, shoptype=False):
+    title = target + " Distribution"
+    if daily:
+        df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
+        df['Day'] = df['Date'].dt.weekday_name
+
+    if storeID != 0:
+        df = df[df["StoreID"] == storeID]
+        title = title + " - Shop" + str(storeID)
+
+    if daily:
+        sb.distplot(a=df[df["Day"] == "Monday"][target], label="Monday").set_title(title)
+        sb.distplot(a=df[df["Day"] == "Tuesday"][target], label="Tuesday").set_title(title)
+        sb.distplot(a=df[df["Day"] == "Wednesday"][target], label="Wednesday").set_title(title)
+        sb.distplot(a=df[df["Day"] == "Thursday"][target], label="Thursday").set_title(title)
+        sb.distplot(a=df[df["Day"] == "Friday"][target], label="Friday").set_title(title)
+        sb.distplot(a=df[df["Day"] == "Saturday"][target], label="Saturday").set_title(title)
+        #sb.distplot(a=df[df["Day"] == "Sunday"][target]), label="Sunday").set_title(title)
+    elif shoptype:
+        sb.distplot(a=df[df["StoreType_Hyper Market"] == 1][target], label="Hyper Market").set_title(title)
+        sb.distplot(a=df[df["StoreType_Super Market"] == 1][target], label="Super Market").set_title(title)
+        sb.distplot(a=df[df["StoreType_Standard Market"] == 1][target], label="Standard Market").set_title(title)
+        sb.distplot(a=df[df["StoreType_Shopping Center"] == 1][target], label="Shopping Center").set_title(title)
+    else:
+        sb.distplot(a=df[target]).set_title(title)
+    plt.legend()
+    plt.show()
+
+
+def scattertargets(df, hue):
+    if hue == "Day":
+        df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
+        df['Day'] = df['Date'].dt.weekday_name
+
+    sb.lmplot(x="NumberOfCustomers", y="NumberOfSales", data=df, hue=hue)
+    plt.show()
 
 if __name__ == '__main__':
     import dataset.dataset as d
